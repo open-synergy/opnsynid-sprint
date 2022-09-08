7506343906f93ad3712272ac15b84724f2cb6a66
Andhitia Rama <andhitia.r@gmail.com>
2022-09-08 15:41:42 +0700

    [sprint_account_invoice_customer_code] 8.0.1.0.0

---
+++ sprint_account_invoice_customer_code/models/account_invoice.py
@@ -0,0 +1,15 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# pylint: disable=W0622
+
+from openerp import fields, models
+
+
+class AccountInvoice(models.Model):
+    _name = "account.invoice"
+    _inherit = "account.invoice"
+
+    customer_code = fields.Char(
+        string="Customer Code",
+    )
