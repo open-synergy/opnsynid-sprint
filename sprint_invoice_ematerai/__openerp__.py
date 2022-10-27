cbcb1efd8f8fae017e24d51745d2e9110557855e
mikevhe18 <viriyananda.michael@gmail.com>
2022-10-27 09:48:38 +0700

    [sprint_invoice_ematerai] 8.0.1.0.0

---
+++ sprint_invoice_ematerai/__openerp__.py
@@ -0,0 +1,21 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice E-Materai",
+    "version": "8.0.1.0.0",
+    "license": "LGPL-3",
+    "category": "Invoicing",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "account",
+        "sprint_ematerai",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
