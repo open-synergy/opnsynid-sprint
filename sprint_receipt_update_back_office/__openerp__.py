fb3f47708f5319ad06c00cdc8ecb6933da3a9ca6
mikevhe18 <viriyananda.michael@gmail.com>
2023-01-18 19:01:51 +0700

    [sprint_receipt_update_back_office] 8.0.1.0.0

---
+++ sprint_receipt_update_back_office/__openerp__.py
@@ -0,0 +1,22 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "SPRINT - Receipt Update Back Office",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Invoicing",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "sprint_back_office",
+        "account_invoice_last_payment",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_config_setting_views.xml",
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
---
+++ sprint_receipt_update_back_office/models/__init__.py
@@ -0,0 +1,10 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import (
+    res_company,
+    res_config_settings,
+    account_invoice_update_payment_history,
+    account_invoice,
+    account_move_line,
+)
