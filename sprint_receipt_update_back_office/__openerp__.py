5b7c3c2c2001721a7bb018efa1963b9a0677429d
mikevhe18 <viriyananda.michael@gmail.com>
2023-01-19 14:28:23 +0700

    [sprint_receipt_update_back_office] 8.0.1.0.2

--- sprint_receipt_update_back_office/__openerp__.py
+++ sprint_receipt_update_back_office/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "SPRINT - Receipt Update Back Office",
-    "version": "8.0.1.0.1",
+    "version": "8.0.1.0.2",
     "license": "AGPL-3",
     "category": "Invoicing",
     "website": "https://simetri-sinergi.id",
--- sprint_receipt_update_back_office/models/account_invoice.py
+++ sprint_receipt_update_back_office/models/account_invoice.py
@@ -1,7 +1,6 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-import json
 
 import requests
 from openerp import _, api, fields, models
@@ -48,11 +47,11 @@ class AccountInvoice(models.Model):
         obj_history = self.env["account.invoice_update_payment_history"]
         base_url = self.company_id.sp_backoffice_url
         url = base_url + self.company_id.sp_update_payment
-        payload = json.dumps(self._prepare_update_payment_data())
+        params = self._prepare_update_payment_data()
         headers = {}
 
         try:
-            response = requests.request("POST", url, headers=headers, data=payload)
+            response = requests.request("POST", url, headers=headers, params=params)
         except requests.exceptions.Timeout:
             msg_err = _("Timeout: the server did not reply within 30s")
             resp_code = "TO"
