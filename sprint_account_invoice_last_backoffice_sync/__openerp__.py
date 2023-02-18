e46f0c1ff112881147dbf5b060033e0f279c31c1
mikevhe18 <viriyananda.michael@gmail.com>
2023-02-18 13:07:44 +0700

    [sprint_account_invoice_last_backoffice_sync] 8.0.1.1.0

--- sprint_account_invoice_last_backoffice_sync/__openerp__.py
+++ sprint_account_invoice_last_backoffice_sync/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "SPRINT - Account Invoice Last Backoffice Sync",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "license": "AGPL-3",
     "category": "Invoicing",
     "website": "https://simetri-sinergi.id",
--- sprint_account_invoice_last_backoffice_sync/models/account_invoice.py
+++ sprint_account_invoice_last_backoffice_sync/models/account_invoice.py
@@ -1,7 +1,7 @@
 # Copyright 2023 OpenSynergy Indonesia
 # Copyright 2023 PT. Simetri Sinergi Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class AccountInvoice(models.Model):
@@ -11,3 +11,38 @@ class AccountInvoice(models.Model):
         string="Last Backoffice Sync.",
         readonly=True,
     )
+    last_tax_computation = fields.Datetime(
+        string="Last Tax Computation",
+        readonly=True,
+    )
+
+    @api.depends(
+        "last_tax_computation",
+        "last_backoffice_sync",
+    )
+    def _compute_need_tax_computation(self):
+        for record in self:
+            if not record.last_tax_computation:
+                record.need_tax_computation = True
+            else:
+                if not record.last_backoffice_sync:
+                    record.need_tax_computation = False
+                elif record.last_backoffice_sync > record.last_tax_computation:
+                    record.need_tax_computation = True
+                else:
+                    record.need_tax_computation = False
+
+    need_tax_computation = fields.Boolean(
+        string="Need Tax Computation",
+        compute="_compute_need_tax_computation",
+        store=True,
+    )
+
+    @api.multi
+    def button_reset_taxes(self):
+        _super = super(AccountInvoice, self)
+        res = _super.button_reset_taxes()
+        for document in self:
+            document.last_tax_computation = fields.Datetime.now()
+
+        return res
