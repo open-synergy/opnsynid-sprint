78ec5169fc682fbf804d3132a4d09a7adcc85392
mikevhe18 <viriyananda.michael@gmail.com>
2023-04-03 10:39:34 +0700

    [sprint_account_invoice_total_qty] 8.0.1.0.0

---
+++ sprint_account_invoice_total_qty/__openerp__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Total QTY On Customer Invoice",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+    "auto_install": False,
+}
