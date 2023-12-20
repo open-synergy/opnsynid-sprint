d31c553a2a6ff64cc61e6d16fa9c2875895fd48e
mikevhe18 <viriyananda.michael@gmail.com>
2023-12-20 18:22:07 +0700

    [ssi_sprint_backoffice_account_move] 14.0.1.0.0

---
+++ ssi_sprint_backoffice_account_move/models/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    sprint_backoffice_backend,
+    account_move_cancel_payment_history,
+    account_move_update_payment_history,
+    account_move_update_print_info,
+    account_move,
+    account_move_line,
+)
